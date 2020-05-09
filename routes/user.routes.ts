1933b03ad3bbeb0961b3d537dc122a41df226690
Vĩnh Hải <gada121982@gmail.com>
2020-05-09 19:51:22 +0700

    Enter

--- routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import express, { Application } from "express";
-import user from "../controllers/user.ctl";
-
-const app: Application = express();
-
-app.get("/", user.getIndex);
-
-export default app;
